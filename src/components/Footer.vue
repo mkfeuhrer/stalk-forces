238c04ae88761e61767a9c02ebd5e75f4ffb9e0f
Mohit Khare <mohitfeuhrer@gmail.com>
2018-12-25 09:31:57 +0530

    Initial Commit - Home page done

---
+++ src/components/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <!-- Don't judge me over some inline - thats just lazy me! -->
+    <footer>
+      Built with
+      <span style="color:red">&#10084;</span> by
+      <a style="color:#555555;" href="https://github.com/mkfeuhrer" target="blank">mkfeuhrer</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+footer {
+  position: fixed;
+  height: auto;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  padding: 20px;
+  z-index: -1;
+  background: #ffffff;
+}
+
+a {
+  color: rgb(0, 0, 0);
+  text-decoration: none;
+  display: inline-block;
+  position: relative;
+}
+a:after {
+  background: none repeat scroll 0 0 transparent;
+  bottom: 0;
+  content: "";
+  display: block;
+  height: 2px;
+  left: 50%;
+  position: absolute;
+  background: rgb(0, 0, 0);
+  transition: width 0.3s ease 0s, left 0.3s ease 0s;
+  width: 0;
+}
+a:hover:after {
+  width: 100%;
+  left: 0;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <img src="./../assets/Stalk-Forces_logo.jpg">
+
+    <p class="foot">Explore what coders are solving!</p>
+    <!-- Search Box -->
+    <SearchBox></SearchBox>
+    <!-- Footer -->
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import SearchBox from "./SearchBox";
+import Footer from "./Footer";
+export default {
+  name: "Home",
+  components: {
+    SearchBox,
+    Footer
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+@import "./../assets/css/bootstrap.min.css";
+.foot {
+  color: #777777;
+  font-size: 2.5vh;
+  padding-top: 20px;
+  padding-bottom: 10px;
+}
+
+.home {
+  padding-top: 20px;
+}
+</style>
